f678ec8d65aa1d6802eb09994748071ed0ded38d
Aleksandr Slyvka <alexandrslivka@gmail.com>
2023-03-26 10:41:05 +0200

    add get function

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { refs } from "./refs";
-import { saveData } from "./api";
+import { saveData, getData } from "./api";
+import { createCard } from "./markup";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
-refs.form.addEventListener('submit', onSubmit);
+refs.form.addEventListener("submit", onSubmit);
 
 async function onSubmit(e) {
-  e.preventDefault()
+  e.preventDefault();
 
   // const { number, name, email } = e.target.elements;
   // const data = {
@@ -18,9 +20,30 @@ async function onSubmit(e) {
   try {
     const data = Object.fromEntries(new FormData(e.target));
     e.target.reset();
-    data.createdAt = Date.now()
+    data.createdAt = Date.now();
     await saveData(data);
+    const markup = createCard([data]);
+    addCard(markup);
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-};
+}
+
+async function init() {
+  try {
+    const response = await getData();
+    if (!response.length) {
+      return;
+    }
+    const markup = createCard(response);
+    addCard(markup);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function addCard(markup) {
+  refs.box.insertAdjacentHTML("beforeend", markup);
+}
+
+init();
