d31ca283d2923bfffb355c46cc9e0d0cc1629fe6
stark1269 <stark1269@gmail.com>
2023-03-26 10:59:09 +0300

    add save data function

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+const BASE_URL = 'http://localhost:3000/contacts';
+
+export function saveData(data) {
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  })
+};
